41189efa1a64f3079a4d781243f64fbc8787a3f2
wesley.christiaens <wesley.christiaens@student.pxl.be>
2023-03-14 15:38:57 +0100

    Add RSA_Encryption XAML controls

---
+++ WPFEncryptionTool2PROA04/RSA_Encryption.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFEncryptionTool2PROA04
+{
+    /// <summary>
+    /// Interaction logic for RSA_Encryption.xaml
+    /// </summary>
+    public partial class RSA_Encryption : Window
+    {
+        public RSA_Encryption()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
