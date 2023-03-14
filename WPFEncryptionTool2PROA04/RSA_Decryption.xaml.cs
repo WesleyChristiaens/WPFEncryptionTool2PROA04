bef4c0c9d5aaa49f52c28a8eaa489aa946c418ee
wesley.christiaens <wesley.christiaens@student.pxl.be>
2023-03-14 15:48:28 +0100

    Add RSA_Decryption XAML

---
+++ WPFEncryptionTool2PROA04/RSA_Decryption.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFEncryptionTool2PROA04
+{
+    /// <summary>
+    /// Interaction logic for RSA_Decryption.xaml
+    /// </summary>
+    public partial class RSA_Decryption : Window
+    {
+        public RSA_Decryption()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
