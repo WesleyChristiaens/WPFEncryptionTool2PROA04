a7279df82428713f67fda90da73ef9b1842c6e48
wesley.christiaens <wesley.christiaens@student.pxl.be>
2023-03-16 00:20:16 +0100

    Add Model Folder

---
+++ WPFEncryptionTool2PROA04/Models/Folder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFEncryptionTool2PROA04.Models
+{
+    public class Folder
+    {
+        public string Name { get; set; }
+        public string path { get; set; }
+    }
+}
