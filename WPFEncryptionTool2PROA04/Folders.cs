93a77f95225597419506aac459429a696881b1c1
wesley.christiaens <wesley.christiaens@student.pxl.be>
2023-03-18 01:37:57 +0100

    Add Folders
    When foldername input is needed use Foldersclass to prevent typo's

---
+++ WPFEncryptionTool2PROA04/Folders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Packaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFEncryptionTool2PROA04
+{
+    public class Folders
+    {
+        public static string FolderIndex = Path.Combine(Environment.CurrentDirectory, "DefaultFolders.csv");
+        public const string GeneratedAESKeys = "GeneratedAESKeys";
+        public const string DecryptedAESKeys = "DecryptedAESKeys";
+        public const string RSAEncryptedAESKeys = "RSAEncryptedAESKeys";
+        public const string AESEncryptedImages = "AESEncryptedImages";
+        public const string Images = "Images";
+        public const string RSAPublicKeys = "RSAPublicKeys";
+        public const string RSAPrivateKeys = "RSAPrivateKeys";
+    }
+}
