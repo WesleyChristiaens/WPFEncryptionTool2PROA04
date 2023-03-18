6022b79b953965095f4a186de840ffe12ad59c18
wesley.christiaens <wesley.christiaens@student.pxl.be>
2023-03-18 15:10:44 +0100

    Update MainWindow Logic

--- WPFEncryptionTool2PROA04/MainWindow.xaml.cs
+++ WPFEncryptionTool2PROA04/MainWindow.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
 
 namespace WPFEncryptionTool2PROA04
@@ -18,27 +12,16 @@ namespace WPFEncryptionTool2PROA04
         {
             InitializeComponent();
         }
-
-        string generatedAESKeysFolder;
-
-
+             
         private async void Window_Loaded(object sender, RoutedEventArgs e)
-        {            
-
+        {  
             if (!File.Exists(Folders.FolderIndex))
             {
                 var file = File.Create(Folders.FolderIndex);                    
                 file.Close();                
             }
 
-            var content = File.ReadAllText(Folders.FolderIndex);           
-
-            if (content.Contains(Folders.GeneratedAESKeys))
-            {
-                generatedAESKeysFolder = FileHelper.GetFolderPath(Folders.GeneratedAESKeys);
-            }
-
-
+            FileHelper.GetKeys(Folders.FolderIndex);
         }
 
 
@@ -55,13 +38,15 @@ namespace WPFEncryptionTool2PROA04
         }
 
         private void BtnGenerateRSA_Click(object sender, RoutedEventArgs e)
-        {
+        {           
+            Encryptor.GenerateNewRSaKeypair(TxtName.Text);
+
 
         }
 
         private void BtnGenerateAES_Click(object sender, RoutedEventArgs e)
-        {
-            CreateAesKey();
+        {            
+            Encryptor.GenerateNewAESKey(TxtName.Text);
         }
 
 
@@ -77,35 +62,20 @@ namespace WPFEncryptionTool2PROA04
 
         private void AESEncrypt_Click(object sender, RoutedEventArgs e)
         {
-            var wpf = new AES_Encryption();
+            var wpf = new WpfAesEncryption();
             wpf.ShowDialog();
         }
 
         private void RSAEncrypt_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
-        private void CreateAesKey()
-        {
-            using (Aes myAes = Aes.Create())
-            {
-                string key = Convert.ToBase64String(myAes.Key);
-                string iv = Convert.ToBase64String(myAes.IV);
-                string keyName = TxtName.Text; //input field from xaml
-                StringBuilder sb = new StringBuilder();
-                sb.Append($"{keyName};{key};{iv}");
+       
 
-                string fileName = @"C:\Github Repositories\WPFEncryptionTool2PROA04\test.txt"; //path name
-                FileStream fs = new FileStream(fileName, FileMode.Append, FileAccess.Write);
-                StreamWriter sw = new StreamWriter(fs);
+        
 
-                sw.WriteLine(sb);
-
-                sw.Close();
-                fs.Close();
-            }
-        }
+        
 
 
     }
