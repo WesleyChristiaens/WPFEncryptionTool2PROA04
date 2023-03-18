fc7e79dccb6c64affc1fc8118d06831493852a10
wesley.christiaens <wesley.christiaens@student.pxl.be>
2023-03-18 15:07:27 +0100

    Add models AesKey and RsaKeyPair

---
+++ WPFEncryptionTool2PROA04/Models/RsaKeyPair.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFEncryptionTool2PROA04.Models
+{
+    public class RsaKeyPair
+    {
+        public string Name { get; set; }
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
