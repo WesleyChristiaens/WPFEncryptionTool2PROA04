7e779395e544aaa108ac525211546691d715a6b4
wesley.christiaens <wesley.christiaens@student.pxl.be>
2023-03-25 14:30:34 +0100

    RSA_Decryption Add Logic to load comboboxes

--- WPFEncryptionTool2PROA04/RSA_Decryption.xaml.cs
+++ WPFEncryptionTool2PROA04/RSA_Decryption.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPFEncryptionTool2PROA04.Models;
 
 namespace WPFEncryptionTool2PROA04
 {
@@ -22,18 +23,56 @@ namespace WPFEncryptionTool2PROA04
         public RSA_Decryption()
         {
             InitializeComponent();
+            LoadComboboxes();
         }
 
-        {
+        string folderRsaPrivateKeys;
+        string folderEncryptedAESKeys;     
+               
 
         private void BtnCancel_Click(object sender, RoutedEventArgs e)
         {
+            this.Close();
         }
 
        
         private void BtnDecrypt_Click(object sender, RoutedEventArgs e)
         {
+           
+        }      
 
+
+
+        private void LoadComboboxes()
+        {
+            LoadCbo(CboAESKeys, Folders.RSAEncryptedAESKeys);
+            LoadCbo(CboprivateRSAKeys, Folders.RSAPrivateKeys);
+        }
+
+        private void LoadCbo(ComboBox cbo, string folder)
+        {
+            cbo.Items.Clear();
+
+            var path = FileHelper.GetFolderPath(folder);
+            var folderContent = FileHelper.GetDirectoryContent(path);
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                if (folderContent.Count() > 0)
+                {
+                    cbo.ItemsSource = FileHelper.GetDirectoryContent(path);
+                }
+                else
+                {
+                    cbo.Items.Add("no keys generated");
+                    cbo.SelectedIndex = 0;
+                }
+            }
+            else
+            {
+                MessageBoxResult msg = MessageBox.Show
+                    ($"Could not find predefined folder {nameof(folder)}", "Problem", MessageBoxButton.OK);
+            }
         }
     }
 }
